b736e224e3977c40098a80b6fc8eb8a3b0896797
jstntrn <justin.tran2290@gmail.com>
2019-02-01 15:11:47 -0700

    post functionality complete

--- src/component/Wizard/StepThree.js
+++ src/component/Wizard/StepThree.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { updateMortgage, updateDesRent } from './../../ducks/reducer';
+import axios from 'axios';
 
 
 class StepThree extends Component{
+
+    handleCreate(obj){
+        axios.post('/api/house', obj)
+        .then((res) => {console.log('added new property')})
+    }
    
     render(){
-        const { monthly_mortgage, updateMortgage, updateDesRent, desired_rent} = this.props
-        const recRent = monthly_mortgage * 1.25;
+        const { name, address, city, stateName, zipcode, image_url, monthly_mortgage, desired_rent, updateDesRent, updateMortgage} = this.props
+        const recRent = Math.round(monthly_mortgage * 1.25);
+        const addObj = {
+            name: name,
+            address: address,
+            city: city,
+            stateName: stateName,
+            zipcode: zipcode,
+            image_url: image_url,
+            monthly_mortgage: monthly_mortgage,
+            desired_rent: desired_rent
+        }
         return(
             <div>
                 <h2>{`Recommended Rent: $${recRent}`}</h2>
@@ -18,7 +34,7 @@ class StepThree extends Component{
                 <input onChange={(e) => updateDesRent(e.target.value)} value={desired_rent} />
                 <div>
                     <Link to='/wizard/steptwo'><button>Previous Step</button></Link>
-                    <Link to='/'><button>Complete</button></Link>
+                    <Link to='/'><button onClick={() => this.handleCreate(addObj)}>Complete</button></Link>
                 </div>
             </div>
         )
@@ -26,8 +42,14 @@ class StepThree extends Component{
 }
 
 function mapStateToProps(state){
-    const { monthly_mortgage, desired_rent } = state;
+    const { name, address, city, stateName, zipcode, image_url, monthly_mortgage, desired_rent } = state;
     return {
+        name,
+        address,
+        city,
+        stateName,
+        zipcode,
+        image_url,
         monthly_mortgage,
         desired_rent
     }
